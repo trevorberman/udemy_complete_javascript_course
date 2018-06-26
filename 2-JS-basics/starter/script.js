42639ec165223a73c49ce1d815efe18cfbd74872
trevorberman <trevorberman@gmail.com>
2018-06-26 16:44:17 -0500

    Add and test primative variables.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!!!');
+// console.log('Hello World!!!');
+var firstName = 'Trevor';
+console.log(firstName);
+
+var lastName = 'Berman';
+var age = 41;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Digital Folk Artist'
+console.log(job);
