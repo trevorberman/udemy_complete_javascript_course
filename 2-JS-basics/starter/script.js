f1984197fae74ba154bcb739dba738f450bc1c5c
trevorberman <trevorberman@gmail.com>
2018-06-26 17:11:03 -0500

    Practice variable coercion, mutation, and alerting and prompting the user.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,15 @@
+// Single line comment example
 // console.log('Hello World!!!');
+
+/*
+Multi-line comment example...
+...for more than one line
+*/
+/****************************
+* Variables and data types
+*/
+
+/*
 var firstName = 'Trevor';
 console.log(firstName);
 
@@ -13,3 +24,29 @@ console.log(job);
 
 job = 'Digital Folk Artist'
 console.log(job);
+*/
+
+/****************************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'Trevor';
+var age = 41;
+
+// Convert num to string to join them.
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Digital Folk Artist';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation redefines or changes value in variables
+age = 'forty one'
+job = 'Sk8er'
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
