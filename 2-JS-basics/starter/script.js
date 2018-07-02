406cb418b00bc47892c49d01398151491b3f5163
trevorberman <trevorberman@gmail.com>
2018-07-02 16:28:54 -0500

    Add completed if/else statement and code challenge #1 revisited sections.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -126,12 +126,11 @@ console.log(x);
 /****************************
 * Coding Challenge 1
 */
-
 /*
 Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height).
 ** Mass in kg, Height in meters
 */
-
+/*
 // 1. Store Mark's and Jon's mass and height in variables.
 var massMark, heightMark, massJohn, heightJohn;
 massMark = 100;
@@ -144,9 +143,63 @@ var bmiMark, bmiJohn;
 bmiMark = massMark / (heightMark * heightMark);
 bmiJohn = massJohn / (heightJohn * heightJohn);
 console.log(bmiMark, bmiJohn);
+*/
 
 /* 3.Create a boolean variable containing information about whether Mark has a higher BMI than John. */
-var compareBMI = bmiMark > bmiJohn;
+// var compareBMI = bmiMark > bmiJohn;
 
 /* 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). */
-console.log('Is Mark\'s BMI higher than John\'s? ' + compareBMI);
+// console.log('Is Mark\'s BMI higher than John\'s? ' + compareBMI);
+
+/****************************
+* If / else statements
+*/
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+/****************************
+* Coding Challenge 1 Revisited
+*/
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height).
+** Mass in kg, Height in meters
+*/
+
+// 1. Store Mark's and Jon's mass and height in variables.
+var massMark, heightMark, massJohn, heightJohn;
+massMark = 100;
+heightMark = 10;
+massJohn = 210;
+heightJohn = 12;
+
+// 2. Calculate both their BMIs.
+var bmiMark, bmiJohn;
+bmiMark = massMark / (heightMark * heightMark);
+bmiJohn = massJohn / (heightJohn * heightJohn);
+console.log(bmiMark, bmiJohn);
+
+/* 3.Create a boolean variable containing information about whether Mark has a higher BMI than John. */
+// var compareBMI = bmiMark > bmiJohn;
+
+/* 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). */
+// console.log('Is Mark\'s BMI higher than John\'s? ' + compareBMI);
+
+if (bmiMark > bmiJohn) {
+  console.log('Mark\'s BMI higher than John\'s.');
+} else {
+    console.log('John\'s BMI higher than Mark\'s.');
+}
